095623c7ce76415b0629412dbdbe3bc7ea23b136
Angela <angamezero@gmail.com>
2025-04-30 18:23:18 -0600

    miniProyecto

---
+++ proyectoGRP/proyectoGRP/src/test/java/GRP/proyectoGRP/ProyectoGrpApplicationTests.java
@@ -0,0 +1,13 @@
+package GRP.proyectoGRP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
